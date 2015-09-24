ed60fd3a0c4df6f21fffca506e9f885db5c68ac8
chiwek <chiwek@gmail.com>
2015-09-24 19:10:37 +0200

    HWIBundle added

---
+++ src/RCTBundle/Controller/DefaultController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace RCTBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+
+class DefaultController extends Controller
+{
+    public function indexAction($name)
+    {
+        return $this->render('RCTBundle:Default:index.html.twig', array('name' => $name));
+    }
+}
---
+++ src/RCTBundle/RCTBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace RCTBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class RCTBundle extends Bundle
+{
+}
